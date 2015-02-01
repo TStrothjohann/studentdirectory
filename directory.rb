f7c3e98c5e7608dc09238b00b6360f23f646f790
TStrothjohann <Thomas@strothjohann.net>
2015-02-01 23:12:32 +0000

    replaced the local variable students with @students which solved the adding-to-list-problem.

--- directory.rb
+++ directory.rb
@@ -1,20 +1,19 @@
+@students= []
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit Enter twice"
-  #create an empty array
-  students=[]
   #get the first name
   name=gets.chomp
   # wehile the name is not empty repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {:name => name, :cohort => :february}
-    puts "Now we have #{students.length} students"
+    @students << {:name => name, :cohort => :february}
+    puts "Now we have #{@students.length} students"
     #get another student's name
     name = gets.chomp
   end
   #return array of students
-  students
+  @students
 end
 
 def print_header
@@ -22,36 +21,44 @@ puts "The students of my cohort at Makers Academy:"
 puts "--------"
 end
 
-def print(students)
-  students.each do |student|
+def print_students
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall we have #{names.length} great students"
+def print_footer
+  puts "Overall we have #{@students.length} great students"
   puts ""
 end
+
+def show_students
+print_header
+      print_students
+      print_footer
+end
+
+def print_menu
+puts "1. input a student"
+    puts "2. show the list of students"
+    puts "9. Exit"
+end
+
 
 #Interactive menu
 
 def interactive_menu
-  students = []
   loop do
   # 1. Show the menu and ask the user what to do
-    puts "1. input a student"
-    puts "2. show the list of students"
-    puts "9. Exit"
+    print_menu
   # 2. get the user input and save it to an variable
     userchoice = gets.chomp
   # 3. Do what the user wants to do
     case userchoice
     when "1"
-      students = input_students
+      @students = input_students
       when "2"
-      print_header
-      print(students)
-      print_footer(students)
+      show_students
       when "9"
       exit #will terminate the program
     else
