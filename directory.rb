9012472029a4df1dc141c57ecaf90e90d03b8464
TStrothjohann <Thomas@strothjohann.net>
2015-02-01 22:44:22 +0000

    added an interactive menu - with a flaw

--- directory.rb
+++ directory.rb
@@ -30,11 +30,40 @@ end
 
 def print_footer(names)
   puts "Overall we have #{names.length} great students"
+  puts ""
 end
 
-#nothing happens until we call the methods:
-students = input_students
-print_header
-print(students)
-print_footer(students)
+#Interactive menu
+
+def interactive_menu
+  students = []
+  loop do
+  # 1. Show the menu and ask the user what to do
+    puts "1. input a student"
+    puts "2. show the list of students"
+    puts "9. Exit"
+  # 2. get the user input and save it to an variable
+    userchoice = gets.chomp
+  # 3. Do what the user wants to do
+    case userchoice
+    when "1"
+      students = input_students
+      when "2"
+      print_header
+      print(students)
+      print_footer(students)
+      when "9"
+      exit #will terminate the program
+    else
+      puts "I don't know what you meant. Please try again."
+    end
+  end
+end
+
+
+#nothing happens until we call the method:
+interactive_menu
+
+
+
 
