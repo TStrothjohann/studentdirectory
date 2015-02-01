ea5a64718dc83520bbbc6bce721d4941118acc5d
TStrothjohann <Thomas@strothjohann.net>
2015-02-01 21:42:48 +0000

    made the name input interactive

--- directory.rb
+++ directory.rb
@@ -1,17 +1,21 @@
-#Let's put all students into an array:
-students =[ 
-  {:name => "Alex Parkinson", :cohort => :february},
-  {:name => "Arfah Farooq", :cohort => :february},
-  {:name => "Carrie Christenson", :cohort => :february},
-  {:name => "Chris Ward", :cohort => :february},
-  {:name => "Cia Walsh", :cohort => :february},
-  {:name => "Costas Sarris", :cohort => :february},
-  {:name => "Diego Romero", :cohort => :february},
-  {:name => "Ed Brown", :cohort => :february},
-  {:name => "Erik Griffin", :cohort => :february},
-  {:name => "Gabe Arafa", :cohort => :february},
-  {:name => "Hannah Carney", :cohort => :february}
-  ]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit Enter twice"
+  #create an empty array
+  students=[]
+  #get the first name
+  name=gets.chomp
+  # wehile the name is not empty repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {:name => name, :cohort => :february}
+    puts "Now we have #{students.length} students"
+    #get another student's name
+    name = gets.chomp
+  end
+  #return array of students
+  students
+end
 
 def print_header
 puts "The students of my cohort at Makers Academy:"
@@ -29,6 +33,7 @@ def print_footer(names)
 end
 
 #nothing happens until we call the methods:
+students = input_students
 print_header
 print(students)
 print_footer(students)
